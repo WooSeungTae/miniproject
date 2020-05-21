ece4cc1060ad2c9159ed27bb894984a57dc89371
sim.sim <sim@sim.aaa>
2020-05-21 16:31:08 +0900

    header, footer, main. jsp 추가

--- src/main/java/com/nike/controller/HomeController.java
+++ src/main/java/com/nike/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,5 +36,16 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@RequestMapping("header")
+	public String header() {
+		return "sminj/header";
+	}
+	@RequestMapping("footer")
+	public String footer() {
+		return "sminj/footer";
+	}
+	@RequestMapping("main")
+	public String main() {
+		return "sminj/main";
+	}
 }
